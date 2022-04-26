68b13bcbff9f2ecbf8d3615c28352d4579559c5a
Bohdan Bodnar <bodnarb.04@spst.cz>
2022-04-26 16:37:29 +0200

    idk

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,21 @@ namespace OOP03
         {
             InitializeComponent();
         }
+
+        Spotrebic spotrebic = new Spotrebic("Ab-002C-K9", 2);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            spotrebic.Zapni();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            spotrebic.Vypni();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(spotrebic.ToString());
+        }
     }
 }
---
+++ Spotrebic.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP03
+{
+    class Spotrebic
+    {
+        private string kod;
+        private double prikon;
+        private TimeSpan celkovaDobaProvozu;
+        private DateTime okamzikZapnuti;
+        private bool jeVProvozu;
+        public string Kod
+        {
+            get
+            {
+                return kod;
+            }
+            set
+            {
+                string s = value;
+                for (int i = 0; i < s.Length; ++i)
+                {
+                    if (char.IsNumber(s[i])) 
+                    { }
+                    else if (char.IsLetter(s[i]))
+                    {
+                        if (char.IsLower(s[i]))
+                        {
+                            s = char.ToUpper(s[i]) + s.Substring(i + 1);
+                        }
+                    }
+                    else if (s[i] == '-') { }
+                    else
+                    {
+                        s = s.Replace(s[i], '-') + s.Substring(i + 1);
+                    }
+                }
+                kod = s;
+            }
+        }
+        public bool JeVProvozu
+        {
+            get
+            {
+                return jeVProvozu;
+            }
+        }
+        public Spotrebic(string kod, double prikon)
+        {
+            this.kod = kod;
+            this.prikon = prikon;
+        }
+        public void Zapni()
+        {
+            jeVProvozu = true;
+            okamzikZapnuti = DateTime.Now;
+            System.Windows.Forms.MessageBox.Show("Zapnuto");
+        }
+        public void Vypni()
+        {
+            jeVProvozu = false;
+            celkovaDobaProvozu += DateTime.Now - okamzikZapnuti;
+            System.Windows.Forms.MessageBox.Show("Vypnuto");
+        }
+        public double CelkovaSpotreba()
+        {
+            double spotreba = 0;
+            if (JeVProvozu)
+            {
+                spotreba = double.Parse((DateTime.Now - okamzikZapnuti).TotalHours.ToString()) * prikon;
+            }
+            else
+            {
+                spotreba = double.Parse(celkovaDobaProvozu.TotalHours.ToString()) * prikon;
+            }
+            return spotreba;
+        }
+        public override string ToString()
+        {
+            string s = "\nKód spotřebiče je: " + Kod + "\nCelková doba provozu je: " + celkovaDobaProvozu.TotalHours + " sekund\n" + "Celková spotřeba je: " + CelkovaSpotreba() + "Wh";
+            return base.ToString() + s;
+        }
+
+    }
+}
